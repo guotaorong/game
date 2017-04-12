017ef04ed3fc3d1313ddedfaade7576881826bf4
grithope <zhunti99999@gmail.com>
2017-04-12 10:38:14 +0800

    step6

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,8 +9,19 @@ class JobsController < ApplicationController
   end
   end
 
+  # def index
+  # @jobs = Job.where(is_hidden: false).order('created_at DESC')
+  # end
+
   def index
-    @jobs = Job.where(is_hidden: false).order('created_at DESC')
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+            else
+              Job.where(is_hidden: false).order('created_at DESC')
+    end
   end
 
   def new
